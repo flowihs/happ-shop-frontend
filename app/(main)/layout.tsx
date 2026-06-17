dafe62345c8b595cbf74a4b7266bdc37bc2393e6
flowihs <zmulenko500@gmail.com>
2026-06-17 18:29:36 +0300

    init project

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,35 +1,44 @@
 import "./globals.css";
 import "./layout.css";
-import Header from "@/components/header/Header";
-import Sidebar from "@/components/sidebar/Sidebar";
-import {AddPlaylistModal} from "@/components/playlist/AddPlaylistModal";
 
-export const metadata = {
-  title: "MusicShare",
-  description: "Музыкальная платформа",
-};
+export default function MainLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <>
+            <nav className="NavigationMenu">
+                <div className="NavigationMenuLeftWrapper">
+                    <h1 className="NavigationMenuLogo">HappShop</h1>
+                    <button className="NavigationMenuCategory">
+                        <svg
+                            className="NavigationMenuCategoryIcon"
+                            viewBox="0 0 24 24"
+                            xmlns="http://w3.org"
+                        >
+                            <line x1="1" y1="5" x2="20" y2="5" />
+                            <line x1="1" y1="12" x2="20" y2="12" />
+                            <line x1="1" y1="19" x2="20" y2="19" />
+                        </svg>
+                        Категории
+                    </button>
 
-export const viewport = {
-  width: "device-width",
-  initialScale: 1,
-};
+                    <div className="NavigationMenuSearch">
+                        <svg className="NavigationMenuSearchIcon"
+                            xmlns="http://w3.org" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            <circle cx="11" cy="11" r="8"></circle>
+                            <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+                        </svg>
+                        <input
+                            className="NavigationMenuSearchInput"
+                            placeholder="Введите текст..."
+                        />
+                    </div>
+                </div>
 
-export default function MainLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <div className="main-shell">
-      <Header />
-      <div className="container-main">
-        <Sidebar />
-        <div className="RootLayout">
-          <main className="content">{children}</main>
-        </div>
-      </div>
+                <a className="NavigationMenuAuthButton">
+                    Войти
+                </a>
 
-      <AddPlaylistModal />
-    </div>
-  );
+            </nav>
+            { children }
+        </>
+    );
 }
