a53d0564d75c6a61637540f1308cd57e154a377c
Mikadze <asansabatyrov@gmail.com>
2026-06-18 19:53:34 +0300

    feat: Добавление карточки товара

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,7 +1,24 @@
+import ProductCard from "@/components/ProductCard";
+
 export default function Home() {
   return (
       <div>
         <h2>Hello</h2>  
+
+        <ProductCard
+            rating={4}
+            image={""}
+            text={"HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello"}
+            price={"400 ₽"}
+            reviews={4242}
+            oldPrice={"1500 ₽"}
+            promoLabels={"Хит продаж"}
+            discount={"45%"}
+            comments={45}
+            quality={"Хорошее"}
+        />
       </div>
   )
 }
+
+
