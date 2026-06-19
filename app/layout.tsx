91ab08b1a536f607bc82afcee5234f0583c1bff9
Mikadze <asansabatyrov@gmail.com>
2026-06-19 12:20:03 +0300

    feat: добавил resetcss, подключил maincss в проект, добавил для добавления fontscss

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import AuthProvider from "@/components/AuthProvider";
 import type { ReactNode } from "react";
+import "@/assets/css/main.css"
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
